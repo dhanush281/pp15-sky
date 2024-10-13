05c377d12057affefd0da8b04fd6afe114054b59
Lostark13 <tushar9915@gmail.com>
2024-10-13 21:41:26 +0530

    Revert "sky: sepolicy: Import changes for XiaomiParts"
    This reverts commit c0e10a9793f6bd621d42a1226238dc2217bf0af3.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -234,8 +234,8 @@ BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
     $(DEVICE_PATH)/sepolicy/private/xiaomi/devicesettings
 
-SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
-    $(DEVICE_PATH)/sepolicy/public/xiaomi/devicesettings
+#SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
+#    $(DEVICE_PATH)/sepolicy/public/xiaomi/devicesettings
 
 # Vendor Boot
 PRODUCT_COPY_FILES += \
